1fa67c017487d94c2c1600fd10da27f38881147a
Tod Beardsley <tod_beardsley@rapid7.com>
2020-04-24 14:29:14 -0500

    First commit of metasploit.js proper

---
+++ src/metasploit.js
@@ -0,0 +1,34 @@
+/*
+Language: Metasploit Framework Session
+Author: Tod Beardsley <tod_beardsley@rapid.com>
+Category: common
+Description: Metasploit Framework is an open source penetration testing framework, available at https://metasploit.com
+*/
+
+function(hljs) {
+  return {
+    aliases: ['metasploit'],
+    case_insensitive: false,
+    keywords: {
+      keyword: 'msf|10',
+      nomarkup: 'exploit auxiliary post nop encoder' // Helps classification
+    },
+    contains: [
+      {
+        className: 'strong',
+        begin: '^\\[\\*\\]', end: ' ',
+        relevance: 10
+      },
+      {
+        className: 'literal',
+        begin: '^\\[\\+\\]', end: ' ',
+        relevance: 10
+      },
+      {
+        className: 'number',
+        begin: '^\\[[-!]\\]', end: ' ',
+        relevance: 10
+      },
+    ]
+  }
+}
